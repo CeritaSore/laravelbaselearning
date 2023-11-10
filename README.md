fb4607c3bcff0283efecc1eaba4a3846512931ac
Fajar Septianto <fajar23.septianto@gmail.com>
2023-11-10 09:37:42 +0700

    added

--- README.md
+++
@@ -1,6 +0,0 @@
-# laravelbaselearning
-Repositori untuk menyimpan data progres belajar Laravel
-
-# 31 oktober 2023
-- membuat repositori
-- menambah file project laravel
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\FormController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ["title" => "Home"]);
 });
+
+Route::get('/about', function () {
+    return view('about', ["title" => "About"]);
+});
+
+Route::get('/post', function(){
+    return view('post');
+});
+
+Route::get('/login', function () {
+    return view('login', ["title" => "Login"]);
+});
+
+Route::resource("mahasiswa", MahasiswaController::class);
